cf1948b0b10642a0897021a9053c7d2e0343c1b4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-03-23 20:21:59 +0530

    Add middleware setup for logging, JSON parsing, and CORS

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is started at PORT: ${PORT}`);
