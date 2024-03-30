0db5951dbbde30702b019e9725e7dad1d4ee7439
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-03-30 11:14:56 +0530

    Added authentication middleware to profile route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { registerController, loginController, getUserProfileController } = require("../controllers/user.controller.js");
+const { isAuthenticate } = require("../middlewares/authMiddleware.js");
 
 const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
-router.get("/profile", getUserProfileController);
+router.get("/profile", isAuthenticate, getUserProfileController);
 
 module.exports = router;
