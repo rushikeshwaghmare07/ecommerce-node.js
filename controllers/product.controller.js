37eafe1e3cb99624b5b2fe87fc3677d1268b95c5
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-10 09:50:16 +0530

    Implement controller and route for fetching product by ID

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,12 +5,28 @@ const getAllProductController = async (req, res) => {
         const product = await Product.find({})
         res.status(200).json({ success: true, message: "All products fetched successfully.", product });
     } catch (error) {
-        console.log("Error in get all product controller: ", error);
+        console.error("Error in get all product controller: ", error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+};
+
+const getProductByIdController = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product) {
+            return res.status(404).json({ success: false, message: "Product not found." });  
+        }
+        res.status(200).json({ success: true, message: "Product fetched successfully." });
+    } catch (error) {
+        console.error("Error in get single product controller: ", error);
+        if (error instanceof CastError) {
+        return res.status(500).json({ success: false, message: "Invalid ID" });
+        }
         res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 };
 
 module.exports = {
     getAllProductController,
-    getSingleProductController
+    getProductByIdController
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getAllProductController, getSingleProductController } = require("../controllers/product.controller");
+const { getAllProductController, getProductByIdController } = require("../controllers/product.controller");
 
 const router = express.Router();
 
 // GET All Products
 router.get("/get-all", getAllProductController);
+// GET Product by Id
+router.get("/:id", getProductByIdController);
 
 module.exports = router;
