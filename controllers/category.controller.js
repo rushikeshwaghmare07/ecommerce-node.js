6ad0294b51136cb4215dbe52e0fbe750ffd27d87
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-11 13:37:15 +0530

    Add delete category functionality

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category.model.js");
+const Product = require("../models/product.model.js");
 
 const createCategoryController = async (req, res) => {
     try {
@@ -29,7 +30,31 @@ const getAllCategoryController = async (req, res) => {
     }
 };
 
+const deleteCategoryController = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+
+        // Update products with the category ID to remove the category reference
+        await Product.updateMany({ category: categoryId }, { $unset: { category: 1 } });
+        
+        // delete the category
+        const deletedCategory = await Category.findByIdAndDelete(categoryId);
+        if (!deletedCategory) {
+            return res.status(404).json({ success: false, message: "Category Not Found !!" });
+        }
+
+        res.status(200).json({ success: true, message: "Category deleted successfully." });
+    } catch (error) {
+        console.error("Error in get all category controller: ", error);
+        if (error.name === "CastError") {
+            return res.status(400).json({ success: false, message: "Invalid product ID format." });
+        }
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+};
+
 module.exports = {
     createCategoryController,
-    getAllCategoryController
+    getAllCategoryController,
+    deleteCategoryController
 }
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { isAuthenticate } = require("../middlewares/authMiddleware.js");
-const { createCategoryController, getAllCategoryController } = require("../controllers/category.controller.js");
+const { createCategoryController, getAllCategoryController, deleteCategoryController } = require("../controllers/category.controller.js");
 const router = express.Router();
 
 router.post("/create", isAuthenticate, createCategoryController);
 router.get("/get-all", getAllCategoryController);
+router.delete("/delete/:id", isAuthenticate, deleteCategoryController);
 
 module.exports = router;
