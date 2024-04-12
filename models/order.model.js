dc57f03551c65944ecef8c869fafb2eef6ac77c2
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-12 09:21:58 +0530

    Add order schema model

---
+++ models/order.model.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+    {
+        shippingInfo: {
+            address: {
+                type: String,
+                required: [true, "address is required."]
+            },
+            city: {
+                type: String,
+                required: [true, "city name is required."]
+            },
+            country: {
+                type: String,
+                required: [true, "country name is required."]
+            }
+        },
+        orderItems: [
+            {
+                name: {
+                    type: String,
+                    required: [true, "product name is required."]
+                },
+                price: {
+                    type: Number,
+                    required: [true, "product price is required."]
+                },
+                quantity: {
+                    type: Number,
+                    required: [true, "product quantity is required."]
+                },
+                image: {
+                    type: String,
+                    required: [true, "product image is required."]
+                },
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                    require: true
+                }
+            }
+        ],
+        paymentMethod: {
+            type: String,
+            enum: ["COD", "ONLINE"],
+            default: "COD"
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "user id is require."]
+        },
+        paidAt: Date,
+        paymentInfo: {
+            id: String,
+            status: String
+        },
+        itemPrice: {
+            type: Number,
+            required: [true, "item price is require"]
+        },
+        tax: {
+            type: Number,
+            required: [true, "tax price is require"]
+        },
+        shippingPrice: {
+            type: Number,
+            required: [true, "item shipping price is require"]
+        },
+        totalAmount: {
+            type: Number,
+            required: [true, "item total amount is require"]
+        },
+        orderStatus: {
+            type: String,
+            enum: ["processing", "shipping", "delivered"],
+            default: "processing"
+        },
+        deliveredAt: Date
+}, { timestamps: true });
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
