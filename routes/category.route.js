97afa9a68cdbba1f661f669fb8983513866d888e
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-15 18:02:31 +0530

    add isAdmin middleware in the routes

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { isAuthenticate } = require("../middlewares/authMiddleware.js");
+const { isAuthenticate, isAdmin } = require("../middlewares/authMiddleware.js");
 const { createCategoryController, getAllCategoryController, deleteCategoryController, updateCategoryController } = require("../controllers/category.controller.js");
 const router = express.Router();
 
-router.post("/create", isAuthenticate, createCategoryController);
+router.post("/create", isAuthenticate, isAdmin, createCategoryController);
 router.get("/get-all", getAllCategoryController);
-router.delete("/delete/:id", isAuthenticate, deleteCategoryController);
-router.put("/update/:id", isAuthenticate, updateCategoryController);
+router.delete("/delete/:id", isAuthenticate, isAdmin, deleteCategoryController);
+router.put("/update/:id", isAuthenticate, isAdmin, updateCategoryController);
 
 module.exports = router;
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const { getAllProductController, getProductByIdController, createProductController, updateProductController, updateImageController, deleteProductImageController, deleteProductController } = require("../controllers/product.controller");
-const { isAuthenticate } = require("../middlewares/authMiddleware.js");
+const { isAuthenticate, isAdmin } = require("../middlewares/authMiddleware.js");
 const { upload } = require("../middlewares/multer.middleware.js");
 
 const router = express.Router();
 
 router.get("/get-all", getAllProductController);
 router.get("/:id", getProductByIdController);
-router.post("/create", isAuthenticate, upload.single("images"), createProductController);
-router.put("/:id", isAuthenticate, updateProductController);
-router.put("/image/:id", isAuthenticate, upload.single("images"), updateImageController);
-router.delete("/delete-image/:id", isAuthenticate, deleteProductImageController);
-router.delete("/delete-product/:id", isAuthenticate, deleteProductController);
+router.post("/create", isAuthenticate, isAdmin, upload.single("images"), createProductController);
+router.put("/:id", isAuthenticate, isAdmin, updateProductController);
+router.put("/image/:id", isAuthenticate, isAdmin, upload.single("images"), updateImageController);
+router.delete("/delete-image/:id", isAuthenticate, isAdmin, deleteProductImageController);
+router.delete("/delete-product/:id", isAuthenticate, isAdmin, deleteProductController);
 
 module.exports = router;
